9fa5e0568c3f9814623ce396acd11279a24bef46
sudhir044 <sudhirkrverma8@gmail.com>
2026-07-07 04:12:20 +0530

    gitignore is working

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -83,6 +83,7 @@ userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
             _id: this._id,
+            
            
         },
         process.env.REFRESH_TOKEN_SECRET,
